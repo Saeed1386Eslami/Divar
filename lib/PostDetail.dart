cd9fe3fa2058ff16b1a3456148dedeea202e9aed
saeed <iscom1357@yahoo.com>
2024-01-14 13:27:00 +0330

    add comment and Divider

--- lib/PostDetail.dart
+++ lib/PostDetail.dart
@@ -74,31 +74,81 @@ class Detail extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(20),
             child: Text(
-              " توضیحات ", textDirection: TextDirection.rtl,
+              " توضیحات ",
+              textDirection: TextDirection.rtl,
               style: TextStyle(
                 fontSize: textSize,
                 color: textColor,
               ),
             ),
           ),
-        Text(" دیوار در زمینه فروش و خرید محصولات و"
+          Text(
+            " دیوار در زمینه فروش و خرید محصولات و"
             " خدمات مختلف فعالیت می کند و به کاربرانش این امکان را می دهد"
             " تا به راحتی و با قیمت مناسب، کالاهای موردنیازشان را"
             " پیدا کنند."
-            " از جمله محصولاتی که در دیوار به فروش می رسد، می توان... " ,
+            " از جمله محصولاتی که در دیوار به فروش می رسد، می توان... ",
             textDirection: TextDirection.rtl,
-        style: TextStyle(
-          fontSize: textSize2,
-          color:textColor,
-        ),
-        ),
-          Divider(
-            color: Colors.black12,
-            height: 33.7,
-              thickness:6.872
-          )
-
+            style: TextStyle(
+              fontSize: textSize2,
+              color: textColor,
+            ),
+          ),
+          Divider(color: Colors.black12, height: 33.7, thickness: 6.8),
+          Joziat(),
+          Divider(),
+          Joziat2(),
         ]));
+  }
+}
+
+// جزییات اسم مناسبی نیست ولی فعلا این گذاشتم که کار رو جلو ببره. این قسمت هم تست نشده و نمیدونم وقتی ران بشه مثل عکس قرار میگیرن نوشته هاش یا نه
+class Joziat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(17),
+      child: (Row(
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            child: Icon(Icons.question_mark_outlined),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.black26),
+                color: Colors.white54),
+          ),
+          Text(" راهنمای خرید امن "),
+          Text(">")
+        ],
+      )),
+    );
+  }
+}
+
+// این قسمت هم همین طور. بخش پایینی عکس مربوطه هست و نمیدونم که چجور قراره ران بشه
+class Joziat2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(17),
+      child: (Row(
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            child: Icon(Icons.add),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.black26),
+                color: Colors.white54),
+          ),
+          Text(" ثبت تخلف و مشکل آگهی "),
+          Text(">")
+        ],
+      )),
+    );
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,13 @@ import 'package:divar/PostDetail.dart';
 import 'package:divar/post.dart';
 import 'package:flutter/material.dart';
 import 'Category.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   // This widget is the root of your application.
   @override
@@ -30,7 +28,6 @@ class MyApp extends StatelessWidget {
 class FirstPage extends StatelessWidget {
   const FirstPage({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,42 +42,103 @@ class FirstPage extends StatelessWidget {
             const Divider(
               color: Colors.black,
             ),
-        InkWell(
-          child: post(" محصول شماره یک ", "400 تومان" , " امروز " , "https://picsum.photos/250?image=9"),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const Detail(),
-              ),
-            );
-          },
-        ),
-          Divider(
-              color: Colors.black,
+            InkWell(
+              child: post(" محصول شماره یک ", "400 تومان", " امروز ",
+                  "https://picsum.photos/250?image=9"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Detail(),
+                  ),
+                );
+              },
             ),
-            post (" محصول شماره دو " , "700 تومان " , " دیروز " ,'https://picsum.photos/250?image=10',),
             Divider(
               color: Colors.black,
             ),
-            post(" محصول شماره سه " , "300 تومان" , " سه روز پیش " , "https://picsum.photos/250?image=11"),
+            post(
+              " محصول شماره دو ",
+              "700 تومان ",
+              " دیروز ",
+              'https://picsum.photos/250?image=10',
+            ),
             Divider(
               color: Colors.black,
             ),
-            post(" محصول شماره چهار " , " 900 تومان" , " 5 روز پیش " , "https://picsum.photos/250?image=12"),
+            post(" محصول شماره سه ", "300 تومان", " سه روز پیش ",
+                "https://picsum.photos/250?image=11"),
             Divider(
               color: Colors.black,
             ),
-            post(" محصول شماره پنج " , " 100 نومان " , " یک هفته پیش " , "https://picsum.photos/250?image=13"),
+            post(" محصول شماره چهار ", " 900 تومان", " 5 روز پیش ",
+                "https://picsum.photos/250?image=12"),
             Divider(
               color: Colors.black,
             ),
+            post(" محصول شماره پنج ", " 100 نومان ", " یک هفته پیش ",
+                "https://picsum.photos/250?image=13"),
+            Divider(
+              color: Colors.black,
+            ),
+            post2(),
           ],
         ),
       ),
     );
   }
 }
+// پست2 چک نشده ولی فکر کنم بدون باگ کار میکنه ، این رو امروز نوشتم و نتونستم ران شدش رو ببینم
+post2() {
+
+  @override
+  Widget build(BuildContext context) {
+    var textColor = Colors.grey;
+    var textSize = 20.0;
+    var textColor2 = Colors.black;
+    return Padding(
+        padding: const EdgeInsets.all(16),
+        child: SizedBox(
+          height: 200,
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset("assets/images/1.webp" ,
+                  height: 200,
+                  width : 200,
+                ),
+              ),
+              Expanded(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text( "  درب چوبی " , style: TextStyle(
+                          color: textColor2,
+                          fontSize: textSize,
+                        ),
+                        ),
+                        const Spacer(),
+                        Text(" 4 میلیون و 300 هزار تومان " ,
+                          style: TextStyle(
+                            color: textColor,
+                            fontSize: textSize,
+                          ),
+                        ),
+                        Text(
+                          " 11 روز پیش " ,
+                          style: TextStyle(
+                            color: textColor,
+                            fontSize: textSize,
+                          ),
+                        ),
+                      ])),
+            ],
+          ),
+        ));
+  }
+}
+
 
 class SecondPage extends StatelessWidget {
   const SecondPage({super.key});
@@ -90,11 +148,11 @@ class SecondPage extends StatelessWidget {
     return Scaffold(
       body: Center(
           child: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(Icons.arrow_back),
-          )),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: Icon(Icons.arrow_back),
+      )),
       backgroundColor: Colors.amber,
     );
   }
